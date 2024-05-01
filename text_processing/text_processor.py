8e64092b9e3d71bd05f16d6f94f21233f160474a
Maria Akhaladze <axaladzemaria@gmail.com>
2024-05-01 19:56:19 +0400

    Fixed property usage with class hint in text processor

--- text_processing/text_processor.py
+++ text_processing/text_processor.py
@@ -3,7 +3,7 @@ from utils.yaml_processor import YamlProcessor
 
 def find_intersections(lemmas: set) -> set[set]:
     matched_keywords = set()
-    for group in YamlProcessor.groups_data:
+    for group in YamlProcessor.groups_data():
         intersection_keywords = lemmas.intersection(group.keywords)
         intersection_include = lemmas.intersection(group.include_keywords)
         intersection_exclude = lemmas.intersection(group.exclude_keywords)
